334244a5ce0498a82ac28d25ca02756e63aa34e7
FabricioCalvo29 <114312297+FabricioCalvo29@users.noreply.github.com>
2026-02-05 18:58:28 -0600

    Comit inicial del proyecto

---
+++ Proyecto_PWA_Clinica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_PWA_Clinica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
